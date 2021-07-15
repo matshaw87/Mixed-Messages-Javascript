c6dadcdd8c56adbe5d078a966039ed0ac6bcc89d
matshaw87 <86411734+matshaw87@users.noreply.github.com>
2021-07-15 14:18:01 +0100

    Add files via upload

---
+++ mixedMessages.js
@@ -0,0 +1,13 @@
+//Mixed Messages Generator 
+
+// create a function
+const messageGenerator = () => {
+
+    //create an array
+    const messages = [
+        {Matt: ' The TV doesnt work'},
+        {Alex: 'Im tired'},
+        {Thomas: ' The dog ate my cookies'},
+        {Boris: ' Do as I say not as I do'}
+    ]
+}
