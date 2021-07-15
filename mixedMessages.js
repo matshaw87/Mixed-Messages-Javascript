60935d52279b58954c27f6d68bc4dee61b83e3a5
matshaw87 <86411734+matshaw87@users.noreply.github.com>
2021-07-15 14:26:02 +0100

    Add files via upload

--- mixedMessages.js
+++ mixedMessages.js
@@ -10,4 +10,14 @@ const messageGenerator = () => {
         {Thomas: ' The dog ate my cookies'},
         {Boris: ' Do as I say not as I do'}
     ]
+
+    let randomIndex =Math.floor((Math.random()*messages.length));
+
+    let messageObject = messages[randomIndex];
+
+    for(const [key, value] of Object.enteries(messageObject)) {
+        let output = `Whats the matter: ${key}, ${value}`;
+        return output;
+    }
 }
+console.log(messageGenerator());
